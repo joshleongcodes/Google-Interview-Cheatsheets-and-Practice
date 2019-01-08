617b2bac44408a06d0d61546ce3c8799b02d5d01
JacobHinkston <JacobHinkston@Gmail.com>
2019-01-07 23:18:47 -0700

    Added all string and array excercises

---
+++ Exercises/ArrayExcercises.java
@@ -0,0 +1,25 @@
+class ArrayExcercises {
+    public static void main(String[] args) {
+        
+    } 
+
+    /*
+     * Interview Question 1.7
+     * Rotate matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+     * write a method to rotate the image by 90 degrees. Can you do this in place?
+     */
+
+    public int[][] rotateMatrix(int[][] matrix){
+        return matrix;
+    }
+
+    /*
+     * Interview Question 1.8
+     * Zero Matrix: Write an algorithm such that an element in an MxN matrix is 0, its entire row and colum are set to 0;
+     */
+
+    public int[][] zeroMatrix(int[][] matrix){
+        return matrix;
+    }
+
+}
---
+++ Exercises/StringExercises.java
@@ -0,0 +1,77 @@
+class StringExcercises {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * Interview Question 1.1
+     * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures.
+     */
+
+    public static boolean isUnique(String str){
+        return false;
+    }
+
+    /*
+     * Interview Question 1.2
+     * Given two strings, write a method to decide if one is a permutation of the other.
+     */
+
+    public static boolean checkPermutation(String a, String b){
+        return false;
+    }
+
+    /*
+     * Interview Question 1.3
+     * Write a method to replace all spaces in a string with '%20'. 
+     * You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string.
+     * Note: If implementing in Java, please use a character array so that you can perform this operation in place.
+     */
+    public static String URLify(char[] charRay){
+        return "";
+    }
+
+    /*
+     * Interview Question 1.4
+     * Palindrone permutation: Given a string, write a function to check if it is a permutation of a palin-drone.
+     * A palindrone is a word or phrase that is the same forwards and backwards. A permutation is a rearrangment of letters.
+     * The palindrone does not need to be limited to just dictationary words.
+     */
+
+    public static boolean palindronePermutation(String str){
+        return false;
+    }
+
+    /*
+     * Interview Question 1.5
+     * One Away: There are three types of edits that can be performed on strings: Insert a character, remove a character, or replace a character. 
+     * Given two strings, write a function to check if they are one eidt (or zero edits) away.
+     */
+
+    public static boolean OneAway(String a, String b){
+        return false;
+    }
+
+    /*
+     * Interview Question 1.6
+     * String compression: Implement a method to perform basic string compression using the couints of repaeated characters.
+     * For example: The string aabcccccaaa would become a2b1c5a3. If the 'compressed' string would not become smaller than the original string, your method should return the original string. 
+     * You can assume that the string has only uppercase and lowercase letters (a-z).
+     * 
+     */
+
+    public static String stringCompression(String str){
+        return "";
+    }
+
+    /*
+     * Interview Question 1.9
+     * Assume you have a method isSubstring which cheks if one word is a substring of another. 
+     * Given two strings, s1 and s2, write code to check is s2 is a rotation of s1 using only one call to isSubstring.
+     * (e.g. "waterbottle" is a rotation of "erbottlewat")
+     */
+
+    public boolean isSubstring(String s1, String s2){
+        return false;
+    }
+}
